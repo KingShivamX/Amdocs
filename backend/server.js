71f37184c42f1474b5866f3724a610b5feed07c8
KingShivamX <shivamhippalgave@gmail.com>
2025-02-02 12:14:54 +0530

    hellowrold

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,35 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const connectDB = require('./config/db');
-const errorHandler = require('./middleware/error');
-const mongoose = require('mongoose');
-const corsOptions = require('./config/cors');
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+const connectDB = require("./config/db")
+const errorHandler = require("./middleware/error")
+const mongoose = require("mongoose")
+const corsOptions = require("./config/cors")
 
 // Initialize express
-const app = express();
+const app = express()
 
 // Connect to database
-connectDB();
+connectDB()
 
 // Middleware
-app.use(cors(corsOptions));
-app.use(express.json());
-app.use(morgan('dev'));
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(morgan("dev"))
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
 
 // Routes
-app.use('/api/auth', require('./routes/auth.routes'));
+app.use("/api/auth", require("./routes/auth.routes"))
 
 // Error Handler (Should be last piece of middleware)
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    console.log(`Server running on port ${PORT}`)
+})
